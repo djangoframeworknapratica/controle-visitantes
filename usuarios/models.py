56de8d95d9ad28a291890d7bdd4e5d146d279afe
Thiago R. Brasil <tchaguitos@gmail.com>
2021-02-25 15:33:37 -0300

    Escrevendo um manager personalizado

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,6 +5,39 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+class UsuarioManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        usuario = self.model(
+            email=self.normalize_email(email),
+        )
+
+        usuario.is_active = True
+        usuario.is_staff = False
+        usuario.is_superuser = False
+
+        if password:
+            usuario.set_password(password)
+
+        usuario.save()
+
+        return usuario
+    
+    def create_superuser(self, email, password):
+        usuario = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+        )
+        
+        usuario.is_active = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+        
+        usuario.set_password(password)
+        usuario.save()
+        
+        return usuario
+
 class Usuario(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(
@@ -29,6 +62,8 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     )
     
     USERNAME_FIELD = "email"
+
+    objects = UsuarioManager()
     
     class Meta:
         verbose_name = "Usuário"
