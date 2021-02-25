465e3d99698147f85191db5d018b7f92e10a1e62
Thiago R. Brasil <tchaguitos@gmail.com>
2021-02-25 15:32:34 -0300

    Escrevendo um modelo personalizado de usuários

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class Usuario(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        verbose_name="E-mail do usuário",
+        max_length=254,
+        unique=True,
+    )
+    
+    is_active = models.BooleanField(
+        verbose_name="Usuario ativo?",
+        default=True
+    )
+        
+    is_staff = models.BooleanField(
+        verbose_name="Usuário é da equipe de desenvolvimento?",
+        default=False
+    )
+
+    is_superuser = models.BooleanField(
+        verbose_name="Usuário é um superusuário?"
+        default=False
+    )
+    
+    USERNAME_FIELD = "email"
+    
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        db_table = "usuario"
+    
+    def __str__(self):
+        return self.email
